4dfb1705083bc5ece2c4f116d7dc4385a76c183d
AberrantMedia <github@gerhildt.bulc.club>
2018-01-30 18:51:18 -0500

    Plan for game plot added.

---
+++ plan.py
@@ -0,0 +1,49 @@
+# PLAN.PY: A plan of the features of the game
+# Player controls Main Character Cern, gains allies that follow Cern and aid in battle
+# First Environment: Cern's house (playable)
+# Bed restores full HP and SP
+# Minor random encounters with Enemy: Roach x1-2
+# Blocking the door outside, First Boss: Mom
+# Upon beating Boss: Mom she flees
+# Second Environment: Outside (playable)
+# Outside Cern can only walk on the road
+# Minor random encounters with Enemy: Roach and Enemy: Squirrel x1-2
+# Meet Ally: Lira on the road
+# Minor random encounters with Enemy: Roach and Enemy: Squirrel x1-3, and Enemy: Cat x1
+# Meet Ally: Shane on the road
+# Minor random encounters with Enemy: Roach and Enemy: Squirrel x1-3, Enemy: Cat x1-2, Enemy: Dog x1
+# At the entrence to the next building, Second Boss: Ice Mage
+# Upon beating Boss: Ice Mage, she transforms into Cute Barista
+# Third Environment: Cafe (drawn)
+# Cute Barista will now restore full HP and SP
+# Second Environment: Outside (playable)
+# Minor random encounters continue
+# Meet Ally: Imma on the road
+# Minor random encounters with Enemy: Arctic Roach and Enemy Arctic Squirrel x1-2, Enemy: Cat x1-2, Enemy: Dog x1
+# At the entrence to the next building, Third Boss: Frost Sorceror
+# Upon beating Boss: Frost Sorceror, he transforms into Kind Farmer
+# Fourth Environment: Farm (drawn)
+# Kind Farmer offers to dig out the snow bank blocking Grams' House
+# Kind Farmer suggests to return to the Cafe while he completes his offer
+# Second Environment: Outside (playable)
+# Minor random encounters continue
+# Third Environment: Cafe (drawn)
+# Cute Barista will now restore full HP and SP
+# Second Environment: Outside (playable)
+# Minor random encounters continue
+# At the entrance to the next building, Fourth Boss: Mom Reprise
+# Upon beating Boss: Mom Reprise, you can choose to Forgive & Reconcile
+# If you do, Mom turns into Decent Mom
+# If not, Mom flees
+# Fifth Environment: Grams' House (drawn)
+# Gram will restore full HP and SP
+# Second Environment: Outside (playable)
+# Minor random encounters with Enemy: Arctic Roach and Enemy Arctic Squirrel x1-3, Enemy: Arctic Cat x1-2, Enemy: Dog x1
+# At the entrance to the next building, Final Boss: Blizzard Wizard
+# Upon defeating Boss: Blizzard Wizard, you can choose to Forgive & Reconcile
+# If you do, Blizzard Wizard turns into Decent Dad
+# If not, Blizzard Wizard flees
+# At game's end, Cern has a party in Grams' House
+# Allies, Grams, Kind Farmer, and Cute Barista are there
+# If you forgave Mom, Decent Mom is there
+# If you forgave Blizzard Wizard, Decent Dad is there
