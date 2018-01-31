273c4d44011859ea5b7cfedee6d4f066f9266976
AberrantMedia <github@gerhildt.bulc.club>
2018-01-30 19:05:16 -0500

    Comments & first try at Character structures

---
+++ characters.py
@@ -0,0 +1,16 @@
+# Cern
+class Cern:
+	def __init__(self):
+		self.lvl = 1
+		self.atk = 0
+		self.dfs = 0
+		self.hp = 0
+		self.sp = 0
+	def Skills(self,lvl):
+		if self.lvl >= 1:
+			def SelfLove(self):
+				self.hp = self.hp + 0
+
+# Allies - Lira, Shane, Imma, Dawn
+# Minor Enemies - Roach, Squirrel, Cat, Dog
+# Bosses - Mom, Ice Mage, Frost Sorcerer, Mom Reprise, Blizzard Wizard
