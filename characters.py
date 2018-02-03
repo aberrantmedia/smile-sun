b2a0722e2ab925d6c0a21c529700ad504987ae76
AberrantMedia <aberrant@goknba.how>
2018-02-02 23:36:56 -0500

    More added to Cern

--- characters.py
+++ characters.py
@@ -1,6 +1,10 @@
 # Cern
 class Cern:
 	def __init__(self):
+		self.sprite = 1
+		self.spriteLeft = 2
+		self.spriteRight = 3
+		self.spriteUp = 4
 		self.lvl = 1
 		self.atk = 0
 		self.dfs = 0
@@ -10,6 +14,12 @@ class Cern:
 		if self.lvl >= 1:
 			def SelfLove(self):
 				self.hp = self.hp + 0
+	def LevelUp(self):
+		self.lvl += 1
+		self.atk += 0
+		self.dfs += 0
+		self.hp += 0
+		self.sp += 0
 
 # Allies - Lira, Shane, Imma, Dawn
 # Minor Enemies - Roach, Squirrel, Cat, Dog
